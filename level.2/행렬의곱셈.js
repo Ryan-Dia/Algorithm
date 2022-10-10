21031816b5876f0c5623ae64c9fba13127f1ee4d
강철원 <cjfdnjs158@naver.com>
2022-10-10 23:48:48 +0900

    thank

--- level.2/행렬의곱셈.js
+++ level.2/행렬의곱셈.js
@@ -18,3 +18,22 @@ function solution(arr1, arr2) {
 
   return box;
 }
+//수정
+
+function solution(arr1, arr2) {
+  let box = [];
+  let result = [];
+  let sum = 0;
+  for (i = 0; i < arr1.length; i++) {
+    for (k = 0; k < arr2[0].length; k++) {
+      for (j = 0; j < arr1[0].length; j++) {
+        sum += arr1[i][j] * arr2[j][k];
+      }
+      result.push(sum);
+      sum = 0;
+    }
+    box.push(result);
+    result = [];
+  }
+  return box;
+}

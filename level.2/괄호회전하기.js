d84ab5fe8fd8f0f44b67f3c5dc9c46f65c4be5ba
강철원 <cjfdnjs158@naver.com>
2022-10-13 01:43:16 +0900

    update

--- level.2/괄호회전하기.js
+++ level.2/괄호회전하기.js
@@ -17,3 +17,67 @@ function solution(s) {
   }
   return result;
 }
+
+///
+
+function solution(s) {
+  let result = 0;
+  let coco;
+  for (i = 0; i < s.length; i++) {
+    let sum;
+    let big;
+    let middle;
+    let small;
+    for (item of s) {
+      if (s.at(-1) === "{" || s.at(-1) === "[" || s.at(-1) === "(") sum = -1;
+      if (s[0] === "}" || s[0] === ")" || s[0] === "]") {
+        sum = -1;
+      }
+      if (sum < 0) break;
+      if (item === "{") big === undefined ? (big = 1) : big++;
+      else if (item === "[") middle === undefined ? (middle = 1) : middle++;
+      else if (item === "(") small === undefined ? (small = 1) : small++;
+      else if (item === "}") big === undefined ? (sum = -1) : big--;
+      else if (item === "]") middle === undefined ? (sum = -1) : middle--;
+      else if (item === ")") small === undefined ? (sum = -1) : small--;
+    }
+    if (big === 0 && middle === 0 && small === 0) coco = true;
+    let save = s[0];
+    s = s.slice(1);
+    s += save;
+    if (coco) {
+      result++;
+      coco = false;
+    }
+  }
+  return result;
+}
+
+///올바른 풀이법
+function solution(s) {
+  const big = /{}/g;
+  const middle = /\[]/g;
+  const small = /\(\)/g;
+  let array = s.slice();
+  let save;
+  let result = 0;
+  for (i = 0; i < s.length; i++) {
+    while (array.length > 0) {
+      if (array.includes("{}")) array = array.replace(big, "");
+      if (array.includes("[]")) array = array.replace(middle, "");
+      if (array.includes("()")) array = array.replace(small, "");
+      if (
+        !array.includes("()") &&
+        !array.includes("[]") &&
+        !array.includes("{}")
+      )
+        break;
+    }
+    if (array.length === 0) result++;
+    save = s[0];
+    s = s.slice(1);
+    s += save;
+    array = s;
+  }
+  return result;
+}

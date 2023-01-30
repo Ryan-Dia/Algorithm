9474b13f7712e0365f3b63095e5ac6042c0e88ce
ryan-dia <cjfdnjs158@naver.com>
2023-01-31 02:40:51 +0900

    문제추가 : level.1 이상한 문자 만들기

---
+++ level.1/이상한 문자 만들기.js
@@ -0,0 +1,7 @@
+function replacer(match, p1, p2, p3, offset, string) {
+  // p1 is nondigits, p2 digits, and p3 non-alphanumerics
+  console.log(p1, p2, p3);
+  return [p1, p2, p3].join(' - ');
+}
+var newString = 'abc12345#$*%'.replace(/([^\d]*)(\d*)([^\w]*)/, replacer);
+console.log(newString); // abc - 12345 - #$*%

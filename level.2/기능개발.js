deef7ca13f72bcc5c22884f81e61c0d9c07e1fe0
ryan-dia <cjfdnjs158@naver.com>
2023-03-14 23:44:19 +0900

    level.2 기능개발- 풀이 추가

--- level.2/기능개발.js
+++ level.2/기능개발.js
@@ -33,3 +33,31 @@ function solution(progresses, speeds) {
   );
   return countBox;
 }
+
+// 풀이 2
+
+function solution(progresses, speeds) {
+  const result = [];
+  // 각 요일 마다 배포 가능한 개수
+  let deployCount = 0;
+  // 배포되는데 필요한 일수
+  let maxDay = Math.ceil((100 - progresses[0]) / speeds[0]);
+
+  for (let i = 0; i < progresses.length; i++) {
+    // 개별 작업의 배포되는데 필요한 일수
+    const day = Math.ceil((100 - progresses[i]) / speeds[i]);
+
+    // 앞 작업에서 배포에 필요한 일수보다 뒷작업의 일수가 더 적으면 함께 배포된다.
+    if (day <= maxDay) {
+      deployCount++;
+    } else {
+      // 앞 작업에서 배포 일수를 충족하였는데 뒷 작업이 배포 할 수 없는 상황일 때
+      result.push(deployCount);
+      deployCount = 1;
+      maxDay = day;
+    }
+  }
+
+  result.push(deployCount);
+  return result;
+}

3897b2d8307054cbb0bb653f452e19cd7e5c1216
ryan-dia <cjfdnjs158@naver.com>
2023-04-01 23:50:11 +0900

    Add: level.2 카펫 풀이 추가

--- level.2/카펫.js
+++ level.2/카펫.js
@@ -7,3 +7,25 @@ function solution(brown, yellow) {
   }
   return [brown / 2 + 2 - y, y];
 }
+
+// 풀이 추가
+
+function solution(brown, yellow) {
+  if (yellow === 1) return [3, 3];
+  let box = [];
+  for (let i = 0; i < yellow; i++) {
+    let divide = yellow / i;
+    if (Number.isInteger(divide) && divide > 0) {
+      box.push([i, divide]);
+    }
+  }
+  if (box.length === 1) {
+    box = box[0];
+    box.sort((a, b) => b - a);
+    return box.map((x) => +x + 2);
+  }
+  const choice = box.length % 2 ? Math.ceil(box.length / 2) - 1 : box.length / 2;
+
+  const result = box[choice];
+  return result.map((x) => +x + 2).sort((a, b) => b - a);
+}

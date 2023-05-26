20a5d09d28637b5f94068c4484ddedd1c6f9fc83
ryan-dia <cjfdnjs158@naver.com>
2023-05-26 19:39:11 +0900

    feaat : level.1 개인정보 수집 유효기간

--- level.2/가장큰수.js
+++ level.2/가장큰수.js
@@ -1,9 +1,8 @@
 function solution(numbers) {
   const answer = numbers
-    // string으로 먼저 변경
-    .map((numbers) => String(numbers))
     // 두 개의 숫자를 조합했을 때 더 큰 수가 앞으로 가도록 정렬
-    .sort((a, b) => b + a - (a + b))
+    // string으로 더해야 숫자처럼 더해지지않고 옆으로 붙기에 string으로 변경 하고 비교한다.
+    .sort((a, b) => b + '' + a - (a + '' + b))
     // 배열을 문자로 변경
     .join('');
 

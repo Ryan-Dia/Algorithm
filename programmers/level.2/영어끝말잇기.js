a0fc2af689b5cfd26b40348688dfd4c3b9b7c7c0
ryan-dia <cjfdnjs158@naver.com>
2023-06-30 08:20:28 +0900

    add(programmers) : level.2 영어끝말잇기 풀이 추가

--- programmers/level.2/영어끝말잇기.js
+++ programmers/level.2/영어끝말잇기.js
@@ -1,3 +1,34 @@
+// 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12981#
+// 문제 해설 : https://html-jc.tistory.com/434
+
+// 풀이 1
+
+function solution(n, words) {
+  const usedWords = new Set([words[0]]);
+  let beforeLastLetter;
+
+  // 총 3번의 return 존재
+  /*
+      1. 끝말잇기가 성립되지 않을 때
+      2. 이미 사용했던 단어를 또 사용했을 때
+      3. 탈락자가 발생하지 않았을 때
+  */
+
+  for (let i = 1; i < words.length; i++) {
+    beforeLastLetter = words[i - 1].at(-1);
+    if (beforeLastLetter !== words[i][0]) {
+      return [(i + 1) % n || n, Math.ceil((i + 1) / n)];
+    }
+    if (usedWords.has(words[i])) {
+      return [(i + 1) % n || n, Math.ceil((i + 1) / n)];
+    }
+    usedWords.add(words[i]);
+  }
+  return [0, 0];
+}
+
+// 풀이 2
+
 function solution(n, words) {
   let count = 0;
   words.reduce((acc, cur, i, arr) => {

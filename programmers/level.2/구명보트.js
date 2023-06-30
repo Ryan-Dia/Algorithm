f5507895e94151f3bdba4e93db88f30aa2cfa61e
ryan-dia <cjfdnjs158@naver.com>
2023-06-30 17:35:30 +0900

    refactor(programmers) : 풀이 추가

--- programmers/level.2/구명보트.js
+++ programmers/level.2/구명보트.js
@@ -1,16 +1,97 @@
 // 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42885
 
+// 풀이 1
+
 function solution(people, limit) {
   people.sort((a, b) => b - a);
-  let Maxboat = [];
-  let box = [];
-  people.map((x) => (x > limit / 2 ? Maxboat.push([x]) : box.push(x)));
-  for (i = 0, k = Maxboat.length - 1; i < box.length; i++) {
-    if (Maxboat[k] && +Maxboat[k][0] + +box[i] <= limit && Maxboat[k].length < 2) {
-      Maxboat[k].push(box[i]);
-      box.splice(i, 1);
-      k--;
+  let count = 0;
+  const peopleMinWeight = people.at(-1);
+  const peopleMap = new Map();
+  for (let i = 0; i < people.length; i++) {
+    peopleMap.set(people[i], (peopleMap.get(people[i]) | 0) + 1);
+  }
+
+  const boatStatus = {
+    weight: limit,
+    limitCount: 0,
+  };
+
+  while (peopleMap.size) {
+    count += 1;
+    initializeBoatStatus(boatStatus, limit);
+
+    for (const [weight, headcount] of peopleMap) {
+      if (cannotBoardBoat(boatStatus.weight, weight)) continue;
+      updateBoatStatus(boatStatus, peopleMap, weight);
+      if (isBoatOverweight(boatStatus.weight, peopleMinWeight)) break;
+      if (isBoatCapacityFull(boatStatus)) break;
+
+      // 같은 몸무게인 사람이 또 존재할 때
+      if (peopleMap.get(weight)) {
+        if (cannotBoardBoat(boatStatus.weight, weight)) continue;
+        updateBoatStatus(boatStatus, peopleMap, weight);
+        if (isBoatCapacityFull(boatStatus)) break;
+      }
     }
   }
-  return Maxboat.length + Math.ceil(box.length / 2);
+  return count;
+}
+
+function cannotBoardBoat(boatWeight, weight) {
+  return boatWeight < weight;
+}
+
+function updateBoatStatus(boatStatus, peopleMap, weight) {
+  boatStatus.weight -= weight;
+  boatStatus.limitCount += 1;
+  peopleMap.set(weight, peopleMap.get(weight) - 1);
+  if (!peopleMap.get(weight)) {
+    peopleMap.delete(weight);
+  }
+}
+
+function isBoatCapacityFull(boatStatus) {
+  return boatStatus.limitCount === 2;
+}
+
+function initializeBoatStatus(boatStatus, limit) {
+  boatStatus.weight = limit;
+  boatStatus.limitCount = 0;
+}
+// 현재 남은 보트 무게보다 구조해야할 사람들 중에 몸무게 최솟값이 더 클때
+// 더 이상 for문 순회할 필요가 없음
+function isBoatOverweight(boatWeight, peopleMinWeight) {
+  return boatWeight < peopleMinWeight;
+}
+
+// 다른 사람 풀이 1
+
+function solution(people, limit) {
+  let count = 0;
+  let index = 0;
+  people.sort((a, b) => a - b);
+  while (people.length > 0) {
+    const biggest = people.pop();
+    index = 0;
+    const boatWeightRemaining = limit - biggest;
+    while (people[index] <= boatWeightRemaining) index += 1;
+    if (index) people.splice(index - 1, 1);
+    count += 1;
+  }
+  return count;
+}
+
+// 다른 사람 풀이 2
+
+function solution(people, limit) {
+  people.sort((a, b) => a - b);
+  let count = 0;
+  while (people.length) {
+    if (people[0] + people[people.length - 1] <= limit) {
+      people.shift();
+    }
+    people.pop();
+    count += 1;
+  }
+  return count;
 }

5214ee734a3ecef2adb68229ab345fa9783ba983
ryan-dia <cjfdnjs158@naver.com>
2023-08-15 23:58:26 +0900

    feat(programmers): 맞은 풀이 추가

--- programmers/level.1/숫자짝궁.js
+++ programmers/level.1/숫자짝궁.js
@@ -1,5 +1,7 @@
 // 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/131128
 
+// 틀린 풀이
+
 function solution(X, Y) {
   const duplication = {};
   const dictX = generateDict([...X]);
@@ -48,3 +50,55 @@ function generateDict(arr) {
 // 제한
 // x,y 는 0으로 시작하지 않는다.
 // x,y의 짝궁은 상당히 큰 정수일 수 있으므로, 문자열로 반환   => 첨부터 문자열로 다루자
+
+// 맞은 풀이
+
+function solution(X, Y) {
+  const dictX = generateDict([...X]);
+  const dictY = generateDict([...Y]);
+
+  const duplicateNumbers = calculateNumbers(dictX, dictY);
+  const partnerNumbers = getPartnerNumbers(duplicateNumbers);
+
+  // X,Y의 짝궁이 0으로만 구성되어 있을 때
+  const zeroCount = partnerNumbers.match(/0/g);
+  const hasMatchingZeroCountWithPartnerNumbers =
+    partnerNumbers.length === zeroCount?.length;
+  if (zeroCount) {
+    if (hasMatchingZeroCountWithPartnerNumbers) {
+      return '0';
+    }
+  }
+
+  // 문제 조건에 맞게 정렬후 다시 string으로 변환
+  const result = [...partnerNumbers].sort((a, b) => b - a).join('');
+  return result ? result : '-1';
+}
+
+function generateDict(arr) {
+  return arr.reduce((acc, cur, index) => {
+    acc[cur] = (acc[cur] | 0) + 1;
+    return acc;
+  }, {});
+}
+
+function calculateNumbers(dictX, dictY) {
+  const duplication = {};
+  const minLengthDict = dictX.length > dictY.length ? dictY : dictX;
+  const anotherDict = dictX.length > dictY.length ? dictX : dictY;
+
+  for (let number in minLengthDict) {
+    if (minLengthDict[number] && anotherDict[number]) {
+      duplication[number] = Math.min(minLengthDict[number], anotherDict[number]);
+    }
+  }
+  return duplication;
+}
+
+function getPartnerNumbers(duplicateNumbers) {
+  let stringNumbers = '';
+  for (let number in duplicateNumbers) {
+    stringNumbers += number.repeat(duplicateNumbers[number]);
+  }
+  return stringNumbers;
+}

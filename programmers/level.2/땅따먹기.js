5d1ad75470ccfdd6a4e6fb9da89eeb3a13b8f675
ryan-dia <cjfdnjs158@naver.com>
2023-08-20 07:48:42 +0900

    feat(programmers): level.2 땅따먹기 풀이 추가

--- programmers/level.2/땅따먹기.js
+++ programmers/level.2/땅따먹기.js
@@ -1,5 +1,7 @@
 // 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12913
+// 풀이 설명: https://html-jc.tistory.com/683
 
+// 풀이 1
 function solution(land) {
   const length = land.length;
 
@@ -11,4 +13,24 @@ function solution(land) {
   }
 
   return Math.max(...land[length - 1]);
+}
+
+// 풀이 2
+
+function solution(land) {
+  const removeColumnUsed = (arr, index) => (arr[index] = 0);
+  const getMaxScore = (row, index) => {
+    const rowCopy = [...row];
+    removeColumnUsed(rowCopy, index);
+    return Math.max(...rowCopy);
+  };
+
+  for (let i = 1; i < land.length; i++) {
+    land[i][0] += getMaxScore(land[i - 1], 0);
+    land[i][1] += getMaxScore(land[i - 1], 1);
+    land[i][2] += getMaxScore(land[i - 1], 2);
+    land[i][3] += getMaxScore(land[i - 1], 3);
+  }
+
+  return Math.max(...land.at(-1));
 }

f2a0854694c84819148819cfe20c65879a336299
ryan-dia <cjfdnjs158@naver.com>
2024-11-29 01:02:38 +0900

    chore: 프로그래머스폴더내에서 java와 javascript로 분류

--- programmers/javascript/level.3/정수 삼각형.js
+++ programmers/javascript/level.3/정수 삼각형.js
@@ -3,18 +3,18 @@
 
 // python
 
-def solution(triangle):
-    for i in range(1, len(triangle)):
-        for j in range(len(triangle[i])):
-            firstCase = triangle[i - 1][j - 1] if j > 0 else 0
-            secondCase = triangle[i - 1][j] if j < len(triangle[i - 1]) else 0
-
-            if secondCase >= firstCase:
-                triangle[i][j] += secondCase
-            else:
-                triangle[i][j] += firstCase
-
-    return max(triangle[-1])
+// def solution(triangle):
+//     for i in range(1, len(triangle)):
+//         for j in range(len(triangle[i])):
+//             firstCase = triangle[i - 1][j - 1] if j > 0 else 0
+//             secondCase = triangle[i - 1][j] if j < len(triangle[i - 1]) else 0
+//
+//             if secondCase >= firstCase:
+//                 triangle[i][j] += secondCase
+//             else:
+//                 triangle[i][j] += firstCase
+//
+//     return max(triangle[-1])
 
 // js
 
